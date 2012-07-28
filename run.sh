1ac7a7bcf17a2c81f499a5d2dbdb427e8e5eb28b
Cody Brocious <cody.brocious@gmail.com>
2012-07-28 00:11:58 -0400

    Improved runner.

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo 'Browse to http://localhost:8000/'
+if [ `uname` == "Darwin" ]; then
+	( sleep 1; open http://localhost:8000/ ) &
+else
+	echo 'Browse to http://localhost:8000/'
+fi
 python -m SimpleHTTPServer 2>/tmp/scrollanim$$.tmp >&2 && rm /tmp/scrollanim$$.tmp && exit
 if [ `grep KeyboardInterrupt /tmp/scrollanim$$.tmp | wc -l` -eq 0 ]; then
 	cat /tmp/scrollanim$$.tmp
